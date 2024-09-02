dd67bb809770b9cfddd6321ca958860517481d4c
LlordZombie <linus.freistetter@hotmail.com>
2024-09-02 15:15:50 +0200

    juppiduppi fertig

--- src/bin2csv/BinToCSV.java
+++ src/bin2csv/BinToCSV.java
@@ -1,4 +1,24 @@
 package bin2csv;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 public class BinToCSV {
+
+    public static
+    void convertBin2CSV(String oldFilename, String newFileName) throws IOException {
+        try (DataInputStream dis = new DataInputStream(new FileInputStream(oldFilename)); BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(newFileName), StandardCharsets.UTF_8))) {
+
+            byte[] buffer = new byte[80];
+            while (dis.read(buffer) != -1) {
+                String personalNumber = new String(buffer, 0, 10, StandardCharsets.UTF_8).trim();
+                String firstName = new String(buffer, 10, 20, StandardCharsets.UTF_8).trim();
+                String lastName = new String(buffer, 30, 20, StandardCharsets.UTF_8).trim();
+                String email = new String(buffer, 50, 30, StandardCharsets.UTF_8).trim();
+
+                writer.write(personalNumber + ";" + firstName + ";" + lastName + ";" + email);
+                writer.newLine();
+            }
+        }
+    }
 }
---
+++ src/tests/bin2csvTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import bin2csv.BinToCSV;
+
+import java.io.IOException;
+
+public class bin2csvTests {
+    public static void main(String[] args){
+        try {
+            BinToCSV.convertBin2CSV("ressources/people.dat", "outTests/persons.csv"
+
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
